7b939ff566124fcc658c2afd0c62e95c75675039
Donovan Mueller <zotobi@gmail.com>
2020-07-31 11:31:12 -0700

    fix: form autosubmit not working.

--- assets/js/plupload_widget.js
+++ assets/js/plupload_widget.js
@@ -8,19 +8,10 @@
   };
 
   Drupal.plupload_widget.uploadCompleteCallback = function (up, files) {
-    var $this = $("#" + up.settings.container);
-    var $container = $this.closest('.form-managed-file');
-    // If there is submit_element trigger it.
-    var submit_element = drupalSettings.plupload[$this.attr('id')].submit_element;
-    if (submit_element) {
-      // Trigger the upload button.
-      var $button = $container.find(submit_element);
-      var $event = drupalSettings['ajax'][$button.attr('id')]['event'];
-      $button.trigger($event);
-      // Now hide the uploader...
-      // the ajax throbber will show.
-      $(up).hide();
-    }
+    $(up.settings.container)
+      .closest('form')
+      .find('#edit-submit')
+      .click()
   };
 
 })(jQuery);
